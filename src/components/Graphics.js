2e486e9b10e629cec1faa191b4896912b0a7fb5b
FredDlt <fredy.delatorre.frd@gmail.com>
2021-03-03 03:54:06 -0500

    fixed render errors

--- src/components/Graphics.js
+++ src/components/Graphics.js
@@ -3,8 +3,6 @@ import { Line } from 'react-chartjs-2'
 import { edad, getClientes } from '../services/clientes'
 import './styles/GraphicCard.css'
 
-
-
 const Graphics = () => {
   const [datos, setDatos] = useState({})
   const chart = ({ cantidad, edades }) => {
@@ -19,18 +17,22 @@ const Graphics = () => {
   }
   useEffect(() => {
     getClientes().then(cliente => {
-      const datos = cliente.map(persona => edad(persona.fechaNacim))
-      let dict = {}
-      for (let edad of datos) {
-        if (edad in dict) {
-          ++dict[edad]
-        } else {
-          dict[edad] = 1
+      try {
+        const datos = cliente.map(persona => edad(persona.fechaNacim))
+        let dict = {}
+        for (let edad of datos) {
+          if (edad in dict) {
+            ++dict[edad]
+          } else {
+            dict[edad] = 1
+          }
         }
+        const cantidad = Object.values(dict)
+        const edades = Object.keys(dict)
+        chart({ cantidad, edades })
+      } catch (error) {
+        console.log(error)
       }
-      const cantidad = Object.values(dict)
-      const edades = Object.keys(dict)
-      chart({ cantidad, edades })
     })
   }, [])
   return (
--- src/components/ListaClientes.js
+++ src/components/ListaClientes.js
@@ -9,21 +9,25 @@ const ListaClientes = () => {
 
   return (
     <table id='tabla' className='tabla'>
-      <thead>
-        <th>Nombre</th>
-        <th>Apellido</th>
-        <th>F. Nacim</th>
-        <th>Edad</th>
-      </thead>
-      <tbody>
-        {clientes.map(x =>
-          <tr>
-            <td>{x.nombre}</td>
-            <td>{x.apellido}</td>
-            <td>{x.fechaNacim.slice(0, 10)}</td>
-            <td>{edad(x.fechaNacim)}</td>
-          </tr>)}
-      </tbody>
+      {clientes.errno
+        ? <p>{clientes.errno + clientes.code + clientes.sqlMessage}</p>
+        : <>
+          <thead>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>F. Nacim</th>
+            <th>Edad</th>
+          </thead>
+          <tbody>
+            {clientes.map(x =>
+              <tr>
+                <td>{x.nombre}</td>
+                <td>{x.apellido}</td>
+                <td>{x.fechaNacim.slice(0, 10)}</td>
+                <td>{edad(x.fechaNacim)}</td>
+              </tr>)}
+          </tbody>
+        </>}
     </table>
   )
 }
